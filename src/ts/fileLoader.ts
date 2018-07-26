687ae135835461ee38f7b2e9cd7627e312ed5f2c
fwcd <fwcdmail@gmail.com>
2018-07-26 10:06:46 +0300

    Save and Save As

--- src/ts/fileLoader.ts
+++ src/ts/fileLoader.ts
@@ -4,39 +4,56 @@ import { EVENT_BUS } from "./renderer";
 
 export class FileLoader {
 	private model: monaco.editor.ITextModel;
+	private currentFilePath?: string;
 	
 	public constructor(model: monaco.editor.ITextModel) {
 		this.model = model;
 		this.registerOpenButton(document.getElementById("open-file"));
 		this.registerSaveButton(document.getElementById("save-file"));
+		this.registerSaveAsButton(document.getElementById("save-file-as"));
 	}
 	
 	private registerOpenButton(button: HTMLElement): void {
-		button.addEventListener("click", () => {
-			remote.dialog.showOpenDialog(remote.getCurrentWindow(), {
-				properties: ["openFile"]
-			}, files => {
-				if (files && files.length > 0) {
-					this.openFile(files[0]);
-				}
-			});
-		});
+		button.addEventListener("click", () => this.showOpenDialog());
 	}
 	
 	private registerSaveButton(button: HTMLElement): void {
 		button.addEventListener("click", () => {
-			remote.dialog.showSaveDialog(remote.getCurrentWindow(), {}, fileName => {
-				if (fileName) {
-					this.saveFile(fileName);
-				}
-			});
+			console.log(this.currentFilePath);
+			if (this.currentFilePath) {
+				this.saveFile(this.currentFilePath);
+			} else {
+				this.showSaveAsDialog();
+			}
+		});
+	}
+	
+	private registerSaveAsButton(button: HTMLElement): void {
+		button.addEventListener("click", () => this.showSaveAsDialog());
+	}
+	
+	private showOpenDialog(): void {
+		remote.dialog.showOpenDialog(remote.getCurrentWindow(), {
+			properties: ["openFile"]
+		}, files => {
+			if (files && files.length > 0) {
+				this.openFile(files[0]);
+			}
+		});
+	}
+	
+	private showSaveAsDialog(): void {
+		remote.dialog.showSaveDialog(remote.getCurrentWindow(), {}, fileName => {
+			if (fileName) {
+				this.saveFile(fileName);
+			}
 		});
 	}
 	
 	private openFile(filePath: string): void {
 		fs.readFile(filePath, "utf-8", (err, data) => {
 			this.model.setValue(data);
-			EVENT_BUS.fire("changefilepath", filePath);
+			this.changeFilePathTo(filePath);
 		});
 	}
 	
@@ -47,6 +64,13 @@ export class FileLoader {
 		}, err => {
 			if (err) console.log(err);
 		});
-		EVENT_BUS.fire("changefilepath", filePath);
+		this.changeFilePathTo(filePath);
+	}
+	
+	private changeFilePathTo(filePath: string): void {
+		if (!this.currentFilePath || (this.currentFilePath !== filePath)) {
+			this.currentFilePath = filePath;
+			EVENT_BUS.fire("changefilepath", filePath);
+		}
 	}
 }
